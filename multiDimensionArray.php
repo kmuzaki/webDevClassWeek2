832cc785b2905ca589eba8cdfe61d0973d785a64
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-09-08 11:22:10 +0800

    multiDimensionArray

---
+++ multiDimensionArray.php
@@ -0,0 +1,18 @@
+<?php
+$staff = [
+    ["Frederick", 30, "Manager"],
+    ["Lily", 25, "Cashier"],
+    ["Pierre", 28, "Head Chef"],
+    ["Edward", 35, "Waiter"],
+    ["Marcus", 22, "Intern"]
+];
+
+echo $staff[2][1]; //30
+echo "<br>";
+
+echo $staff[1][0]; //Lily
+echo "<br>";
+
+echo $staff[2][2]; //Head Chef
+echo "<br>";
+?>
