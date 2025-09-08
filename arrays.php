1718947859a56d443731a7c5e788a188b369c554
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-09-08 10:04:40 +0800

    forEachArrayLoop

--- arrays.php
+++ arrays.php
@@ -1,10 +1,24 @@
 <?php
-$staff = ["Frederick", "Lily", "Pierre", "Edward", "Marcus"];
+$staff = ["Frederick", "Lily", "Pierre", "Edward", "Marcus", "Ucup"];
 $foods = ["Tempe", "Omelette", "Curry", "Salad", "Pasta"];
 $dynamicArray = ["Nintendo", 64, true, 19.96];
 
 var_dump($staff);
 echo "<br>";
+
 print_r($foods);
 echo "<br>";
+
+print_r($dynamicArray);
+echo "<br>" . "<br>";
+
+echo "There are exactly " . count($staff). " employees in this business. Which are: ";
+foreach($staff as $employee) {
+    if ($employee === end($staff)) {
+        echo "and " . $employee . ".";
+        break;
+    }
+    echo $employee . ", ";
+}  
+
 ?>
