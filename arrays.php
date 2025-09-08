3204d068264f31277400f787f83a776bba05311a
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-09-08 09:52:12 +0800

    changeVarName

--- arrays.php
+++ arrays.php
@@ -1,4 +1,10 @@
 <?php
-$mahasiswa = ["Asep", "Budi", "Tono"];
-echo $mahasiswa[0];
+$staff = ["Frederick", "Lily", "Pierre", "Edward", "Marcus"];
+$foods = ["Tempe", "Omelette", "Curry", "Salad", "Pasta"];
+$dynamicArray = ["Nintendo", 64, true, 19.96];
+
+var_dump($staff);
+echo "<br>";
+print_r($foods);
+echo "<br>";
 ?>
