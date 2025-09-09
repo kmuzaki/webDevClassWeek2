3dcfd9e128df97ba0e55de82f3f00a5736d5a0bc
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-09-09 10:49:47 +0800

    renameFiles

--- themeParkRides.php
+++ themeParkRides.php
@@ -32,3 +32,26 @@ $rides = [
     ]
 ];
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GET Rides</title>
+</head>
+<body>
+    <h1>List of Theme Park Rides</h1>
+    <ul>
+        <?php foreach($rides as $ride) : ?>
+            <li>
+                <a href="associativeArray.php?name=<?php echo $ride["name"]; ?>
+                        &rideNumber=<?php echo $ride["rideNumber"]; ?>
+                        &category=<?php echo $ride["category"]; ?>
+                        &img=<?php echo $ride["img"]; ?>">
+                        <?php echo $ride["name"]; ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+</html>
