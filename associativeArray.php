94cb25f0e0fa67cb09085a2218cbdde74297c542
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-09-09 10:31:35 +0800

    addData

--- associativeArray.php
+++ associativeArray.php
@@ -1,1 +1,34 @@
-<?
+<?php
+$rides = [
+    [
+        "name"=> "Diamond Wheel",
+        "rideNumber"=> "01",
+        "category"=> "calm_rides",
+        "img" => "ferrisWheel.png"
+    ],
+    [
+        "name"=> "The Speed Monsters",
+        "rideNumber"=> "02",
+        "category"=> "thrill_rides",
+        "img" => "goKarts.png"
+    ],
+    [
+        "name"=> "Carousel",
+        "rideNumber"=> "03",
+        "category"=> "calm_rides",
+        "img" => "merryGoRound.png"
+    ],
+    [
+        "name"=> "Iron Giant",
+        "rideNumber"=> "04",
+        "category"=> "Looping Coasters",
+        "img" => "RCLoop.png"
+    ],
+    [
+        "name"=> "Wario's Gold Mine",
+        "rideNumber"=> "05",
+        "category"=> "Wooden Coasters",
+        "img" => "RCWood.png"
+    ]
+];
+?>
